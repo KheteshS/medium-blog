534dd326e5d1355e1d8572e454461d6ef261a9da
KheteshS <kheteshs@gmail.com>
2025-05-27 19:17:47 +0530

    signIn & sign up endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,88 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { decode, sign, verify } from "hono/jwt";
 
-const app = new Hono();
+type Variables = {
+  userId: string;
+};
 
-app.post("/api/v1/user/signup", (c) => {
-  return c.text("signup route");
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: Variables;
+}>();
+
+app.use("/api/v1/blog/*", async (c, next) => {
+  const jwt = c.req.header("Authorization");
+
+  if (!jwt) {
+    return c.json({ error: "Unauthorized!" }, 403);
+  }
+
+  const token = jwt.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    return c.json({ error: "Unauthorized!" }, 403);
+  }
+  const userId = (payload as any).id;
+  c.set("userId", userId);
+  await next();
 });
 
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("signin route");
+app.post("/api/v1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({
+      message: "User created !!",
+      token: token,
+    });
+  } catch (e) {
+    if ((e as any).code === "P2002") {
+      return c.json({ error: "Email already exists" }, 403);
+    }
+    return c.json({ error: "Internal Server error!" }, 500);
+  }
+});
+
+app.post("/api/v1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: body.email },
+    });
+
+    if (!user) {
+      return c.json({ error: "User not found" }, 403);
+    }
+
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({
+      message: "User Logged In !!",
+      token: token,
+    });
+  } catch (e) {
+    return c.json({ error: "Inavlid email or password!" }, 401);
+  }
 });
 
 app.get("/api/v1/blog/:id", (c) => {
